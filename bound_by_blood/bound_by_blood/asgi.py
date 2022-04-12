60f8bee13c57df9443e748bede1520beb53c281c
samanwita-2003 <103487826+samanwita-2003@users.noreply.github.com>
2022-04-12 13:01:50 +0530

    Add files via upload

---
+++ bound_by_blood/bound_by_blood/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for bound_by_blood project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bound_by_blood.settings')
+
+application = get_asgi_application()
---
+++ bound_by_blood/bound_by_blood/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for bound_by_blood project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bound_by_blood.settings')
+
+application = get_wsgi_application()
